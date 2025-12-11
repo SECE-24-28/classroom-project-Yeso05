7039c4145872a7dd2e93086069be0c6f89c96358
Yeso05 <yeswanthiras@gmail.com>
2025-12-11 11:07:09 +0530

    DAY 8 ROUTER_EXA

--- App.jsx
+++ App.jsx
@@ -2,24 +2,36 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { DataProvider } from './DataContext'
-import One from './One'
-import Two from './Two'
-import Three from './Three'
-
-
+import { Link,Route,Routes } from 'react-router-dom'
+import Gallery from './Gallery'
+import NewPost from './NewPost'
+import Post from './Post'
+import PostPage from './PostPage'
+import Home from './Home'
+import About from './About'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className='outer-container'>
-    <DataProvider>
-      <One />
-      <Two /> 
-      <Three />
-    </DataProvider>
-    </div>
+    <>
+     <ul>
+      <li> <Link to="/">Home</Link> </li>
+      <li> <Link to="/about">About</Link> </li>
+      <li> <Link to="/gallery">Gallery</Link> </li>
+      <li><Link to="/postpage">PostPage</Link></li>
+     </ul>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="postpage" element={<PostPage/>}>
+                    <Route path=":id" element={<Post/>}/>
+                    <Route path="newpost" element={<NewPost/>}/>
+      </Route>
+    </Routes>
+  </>  
   )
 }
 
