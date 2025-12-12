405eca8941d0e8bc2a9b2053b0bedf93101c004b
Yeso05 <yeswanthiras@gmail.com>
2025-12-12 15:59:52 +0530

    DAY 9 axios_exa

--- App.jsx
+++ App.jsx
@@ -1,40 +1,33 @@
- xx import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import api from './Api/Stu_api'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [SList,setSList] = useState([])
+   
+  useEffect(()=>{
+    const fetchData = async()=>{
+      try{
+        const res=await api.get("/students")
+        setSList(res.data)
+        //consiole.log(res.data)
 
-
-  useEffect((=>{
-    const tem_list=[
-      {id:1,sname}
-    ]
-  }))
-
+      }catch(err){
+        //console.error(err)
+      }
+    }
+    fetchData()
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {
+      SList.map((stu)=>
+        <p>{stu.sid}-{stu.sname}-{stu.smark}</p>
+    )
+    }
+      
     </>
   )
 }
